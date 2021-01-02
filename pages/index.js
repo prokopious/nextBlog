4229b0f7aa2392aa40c855196b19c137969a6ad9
tmhuyett <toddhuyett@gmail.com>
2021-01-01 21:16:51 -0500

    'fddf'

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Index({ allPosts }) {
       backgroundImage: `url("https://images.unsplash.com/photo-1609262855551-76ff9bb66efe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80")`}}>
   
   
-  <a href="#about">About</a>
-  <a href="#services">Classifier</a>
-  <a href="#contact">Contact</a>
+  <a href="#about">about</a>
+  <a href="#services">classifier</a>
+  <a href="#contact">contact</a>
 </div>
 
     
